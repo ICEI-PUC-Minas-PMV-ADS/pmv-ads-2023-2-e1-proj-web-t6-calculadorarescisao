873a1e119cf9828d45dc23abd875dc86f5295a82
Fábio Barkoski <65479069+fabiobarkoski@users.noreply.github.com>
2023-11-21 19:52:43 -0300

    fix: historico do calculo (#32)

--- docs/static/js/history.js
+++ docs/static/js/history.js
@@ -1,3 +1,5 @@
+import utils from "./utils.js"
+
 const mes = {
   Jan: "Janeiro",
   Feb: "Fevereiro",
@@ -13,30 +15,52 @@ const mes = {
   Dez: "Dezembro",
 }
 
+const motivos = {
+  justacausa: "Dispensa de justa causa",
+  semjustacausa: "Dispensa sem justa causa",
+  pedidodemissao: "Pedido de demissão",
+  demissaocomumacordo: "Demissão de comum acordo",
+  contratonoprazo: "Encerramento de contrato de experiência no prazo",
+  contratoantesprazo: "Encerramento de contrato de experiência antes do prazo",
+  aposentadoria: "Aposentadoria do Empregado",
+  falecimento: "Falecimento do Empregador",
+}
+
+window.addEventListener("load", (event) => {
+  const cards = document.getElementsByClassName("cards-historico");
+  for (let i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", (event) => {
+      console.log('clico')
+      const cardId = utils.getCardId(event.target)
+      console.log(cardId);
+      const cardValue = utils.getCardValue(cardId);
+      console.log(cardValue)
+      if (cardValue){
+        utils.adicionarTabelaRescisao(cardValue);
+      }
+    })
+  }
+});
+
 function dateFormatter(date) {
   const arrayDate = date.split(" ")
   return `${arrayDate[2]} de ${mes[arrayDate[1]]}, ${arrayDate[3]}`
 }
 
-function showValue(value) {
-  document.getElementById("informacoes").textContent = `O valor de sua rescisão será de ${value}`;
-}
-
 function getHistory() {
   if (localStorage.getItem("calculos")) {
     const calculos = JSON.parse(localStorage.getItem("calculos"));
-    const cards = document.getElementById("cards");
+    const historico = document.getElementById("historico");
 
     calculos.valores.forEach(element => {
-      cards.children[0].innerHTML += `
-        <div class="w-96 h-64 shadow-lg hover:shadow-xl" onclick=showValue("${element.valor}")>
+      historico.children[0].innerHTML += `
+        <div class="w-96 h-64 shadow-lg cards-historico hover:shadow-xl" card-id="${element.id}">
           <div class="historico-data bg-blue-500 rounded-t-lg">
             <time datetime="2023-10-24">${dateFormatter(element.data)}</time>
           </div>
-          <div class="text-black">
-            <p>Resultado: ${element.valor}</p>
-            <br>
-            <p>Motivo: ${element.motivo}</p>
+          <div class="historico-texto text-black">
+            <p>Calculo de ${motivos[element.motivo.replace("-", "")]},</p>
+            <p>tendo o valor da rescisão de ${utils.formatarValorMonetario(element.valor.rescisao.total)}</p>
           </div>
         </div>
         `
@@ -44,20 +68,49 @@ function getHistory() {
   }
 }
 
-export function showOnHistory(valor, data, motivo) {
-  const cards = document.getElementById("cards");
-  cards.children[0].innerHTML += `
-        <div class="w-96 h-64 shadow-lg hover:shadow-xl" onclick=showValue("${valor}")>
+function showOnHistory(valor, data, motivo, id) {
+  const historico = document.getElementById("historico");
+  historico.children[0].innerHTML += `
+        <div class="w-96 h-64 shadow-lg cards-historico hover:shadow-xl" card-id="${id}">
           <div class="historico-data bg-blue-500 rounded-t-lg">
             <time datetime="2023-10-24">${dateFormatter(data)}</time>
           </div>
           <div class="text-black">
-            <p>Resultado: ${valor}</p>
+            <p>Calculo de ${motivos[motivo.replace("-", "")]},</p>
             <br>
-            <p>Motivo: ${motivo}</p>
+            <p>Tendo o valor da rescisão de ${valor}</p>
           </div>
         </div>
         `
 } 
+
+export function saveCalc(valoresRescicao, motivo) {
+  let calculos = JSON.parse(localStorage.getItem("calculos")) || {valores: []}
+  const tamanhoValores = calculos.valores.length;
+  if (tamanhoValores === 5) {
+    calculos.valores.splice(0, 1);
+    const cards = document.getElementsByClassName("cards-historico");
+    cards[0].remove();
+  }
+  const dataCalculo = new Date();
+
+  calculos.valores.push({
+    data: dataCalculo.toDateString(),
+    valor: valoresRescicao,
+    motivo: motivo,
+  })
+
+  calculos.valores.forEach((valor, index) => {
+    calculos.valores[index] = {...valor, id: index};
+  });
+
+  localStorage.setItem("calculos", JSON.stringify(calculos));
+  showOnHistory(
+    utils.formatarValorMonetario(valoresRescicao.rescisao.total),
+    dataCalculo.toDateString(),
+    motivo,
+    tamanhoValores
+  );
+}
 
 getHistory()
---
+++ docs/static/js/utils.js
@@ -0,0 +1,152 @@
+const nomenclatura = {
+  verbas: "Verbas Rescisórias",
+  deducoes: "Deduções",
+  fgts: "FGTS",
+  rescisao: "Total Rescisão",
+  saldoSalario: "Saldo de Salário",
+  feriasVencidas: "Férias Vencidas",
+  feriasProps: "Férias Proporcionais",
+  decimoTerceiro: "13° proporcional ",
+  tercoFerias: "1/3 das Férias",
+  inss: "INSS",
+  inssDecimo: "INSS 13°",
+  irrf: "IRRF",
+  depositado: "Depositado",
+  multa: "Multa"
+}
+
+function formatarValorMonetario(numero) {
+  const options = {
+    style: 'currency',
+    currency: 'BRL'
+  };
+  return numero.toLocaleString('pt-BR', options);
+}
+
+function adicionarTabelaRescisao(valoresRescicao) {
+
+  const divResultado = document.getElementById("divInfo");
+  if (document.getElementById("table-resultado")) {
+    document.getElementById("divInfo").removeChild(document.getElementById("table-resultado"))
+  }
+  const tabela = document.createElement("table");
+  tabela.className = "w-full table-auto bg-white shadow-md rounded-lg table-custom";
+  tabela.id = "table-resultado"
+  const thead = document.createElement("thead");
+  thead.innerHTML = `
+  <tr>
+    <th class="px-4 py-2 bg-gray-200 text-gray-700">Descrição</th>
+    <th class="px-4 py-2 bg-gray-200 text-gray-700">Valor (R$)</th>
+  </tr>
+  `;
+
+  const tbody = document.createElement("tbody");
+
+  for (let item in valoresRescicao) {
+    const novaLinha = document.createElement("tr");
+    novaLinha.className = 'resumo'
+    if (item !== 'rescisao'){
+      for (let d in valoresRescicao[item]){
+        const detalhes = document.createElement("tr");
+        detalhes.className = 'detalhes hidden';
+        if (d !== 'total') {detalhes.innerHTML = `
+            <td class="border px-4 py-2">${nomenclatura[d]}</td>
+            <td class="border px-4 py-2">${formatarValorMonetario(valoresRescicao[item][d])}</td>
+          `
+          tbody.appendChild(detalhes);
+        }
+      }
+    }
+    novaLinha.innerHTML = `
+    <td class="border px-4 py-2">${nomenclatura[item]}</td>
+    <td class="border px-4 py-2">${formatarValorMonetario(valoresRescicao[item].total)}</td>
+  `;
+    
+    tbody.appendChild(novaLinha);
+  };
+
+  tabela.appendChild(thead);
+  tabela.appendChild(tbody);
+  if (document.getElementById("informacoes")) {
+    divResultado.removeChild(document.getElementById("informacoes"))
+  }
+
+  if (!document.querySelector('#check')){
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.value = "mais";
+    checkBox.id = "check";
+    checkBox.onclick = () => {
+      if (document.querySelector('input[type="checkbox"]').checked){
+        const linhasOcultas = document.querySelectorAll("tr.detalhes");
+        const linhasResumo = document.querySelectorAll("tr.resumo");
+        removerClasses(linhasOcultas, "hidden");
+        adicionarClasses(linhasResumo, "bg-gray-300");
+        document.querySelector('label').textContent = " Mostrar Menos";
+      } else{
+        const linhasOcultas = document.querySelectorAll("tr.detalhes");
+        const linhasResumo = document.querySelectorAll("tr.resumo");
+        adicionarClasses(linhasOcultas, "hidden");
+        removerClasses(linhasResumo, "bg-gray-300");
+        document.querySelector('label').textContent = " Mostrar Mais";
+      }
+    }
+    const checkLabel = document.createElement("label");
+    checkLabel.innerText = "  Mostrar mais";
+    divResultado.appendChild(checkBox);
+    divResultado.appendChild(checkLabel);
+  }
+
+  divResultado.appendChild(tabela);
+}
+
+function mesesDiferenca(dateAdmissao, dataRecisao) {
+  return dataRecisao.getMonth() - dateAdmissao.getMonth() + 
+      (12 * (dataRecisao.getFullYear() - dateAdmissao.getFullYear()))
+}
+
+function formataData(data){
+  return `${data.split('/')[1]}/${data.split('/')[0]}/${data.split('/')[2]}`
+}
+
+function removerClasses(elementos, valor) {
+  elementos.forEach(e => {
+    e.classList.remove(valor);
+  });
+}
+
+function adicionarClasses(elementos, valor) {
+  elementos.forEach(e => {
+    e.classList.add(valor);
+  });
+}
+
+function getCardId(eventTarget) {
+  if (eventTarget.getAttribute("card-id")){
+    return eventTarget.getAttribute("card-id")
+  } else{
+    getCardId(eventTarget.parentElement)
+  }
+}
+
+function getCardValue(cardId) {
+  const calculos = JSON.parse(localStorage.getItem("calculos"))
+  let retorno = false
+  calculos.valores.forEach(calculo => {
+    if (calculo.id === parseInt(cardId)) {
+      retorno = calculo.valor
+    }
+  })
+  return retorno;
+}
+
+export default {
+  formatarValorMonetario,
+  adicionarTabelaRescisao,
+  mesesDiferenca,
+  formataData,
+  removerClasses,
+  adicionarClasses,
+  getCardId,
+  getCardValue,
+}
